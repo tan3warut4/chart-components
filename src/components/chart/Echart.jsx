62f5e2a690fa3df6498e2e87404a70dc091ff16d
Warut Tan <itanprscritical@gmail.com>
2022-11-23 09:04:04 +0700

    update

--- src/components/chart/Echart.jsx
+++ src/components/chart/Echart.jsx
@@ -41,13 +41,7 @@ const Echart = () => {
         data: [120, 132, 101, 134, 90, 230, 210],
         itemStyle: {color: '#063970'}
       },
-      {
-        name: "Union Ads",
-        type: "line",
-        stack: "Total",
-        data: [220, 182, 191, 234, 290, 330, 310],
-        itemStyle: {color: '#063970'}
-      },
+     
     ],
   };
 
